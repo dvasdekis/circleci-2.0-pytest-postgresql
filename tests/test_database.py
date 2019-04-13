9cb9b673f6f0f6ffd77cc4cf77e3aeff0f2b10f3
dvasdekis <dimitri@vasdekis.com.au>
2019-04-13 20:03:32 +1000

    Changed env var db name to circleci name

--- tests/test_database.py
+++ tests/test_database.py
@@ -6,7 +6,7 @@ from app import my_app
 from app.connect_to_db import connect_to_db
 
 
-DATABASE = os.getenv('DB_NAME')
+DATABASE = os.getenv('DATABASE_NAME')
 
 SQL_QUERY = """
 CREATE SCHEMA trains;
